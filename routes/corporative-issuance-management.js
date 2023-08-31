f8d6344bac88cb0f16aa6a77598eebef3933cd0a
Jhon Alen <jalen@compuamerica.com.ve>
2023-08-31 16:12:24 -0400

    proveedores y exclusion

--- routes/corporative-issuance-management.js
+++ routes/corporative-issuance-management.js
@@ -463,4 +463,34 @@ const operationCorreo = async(authHeader, requestBody) => {
         });
 }
 
+router.route('/exclude').post((req, res) => {
+    if(!req.header('Authorization')){
+        res.status(400).json({ data: { status: false, code: 400, message: 'Required authorization header not found.' } });
+        return;
+    }else{
+        operationExcludePropietary(req.header('Authorization'), req.body).then((result) => {
+            if(!result.status){
+                res.status(result.code).json({ data: result });
+                return;
+            }
+            res.json({ data: result });
+        }).catch((err) => {
+            console.log(err.message);
+            res.status(500).json({ data: { status: false, code: 500, message: err.message, hint: 'operationExcludePropietary' } });
+        });
+    }
+});
+
+const operationExcludePropietary = async(authHeader, requestBody) => {
+    if(!helper.validateAuthorizationToken(authHeader)){ return { status: false, code: 401, condition: 'token-expired', expired: true }; }
+    let updateData = {
+        ccontratoflota: requestBody.ccontratoflota,
+        cestatusgeneral: requestBody.cestatusgeneral
+    };
+    console.log(updateData)
+    let excludePropietary = await bd.excludePropietaryQuery(updateData).then((res) => res);
+    if(excludePropietary.error){ return  { status: false, code: 500, message: excludePropietary.error }; }
+    if(excludePropietary.result.rowsAffected > 0){ return { status: true }; }
+}
+
 module.exports = router;
--- routes/valrep.js
+++ routes/valrep.js
@@ -3362,4 +3362,32 @@ const operationValrepSearchService = async(authHeader, requestBody) => {
     return { status: true, list: jsonArray }
 }
 
+router.route('/status').post((req, res) => {
+    if(!req.header('Authorization')){ 
+        res.status(400).json({ data: { status: false, code: 400, message: 'Required authorization header not found.' } })
+        return;
+    }else{
+        operationValrepStatus(req.header('Authorization'), req.body).then((result) => {
+            if(!result.status){ 
+                res.status(result.code).json({ data: result });
+                return;
+            }
+            res.json({ data: result });
+        }).catch((err) => {
+            res.status(500).json({ data: { status: false, code: 500, message: err.message, hint: 'operationValrepStatus' } });
+        });
+    }
+});
+
+const operationValrepStatus = async(authHeader, requestBody) => {
+    if(!helper.validateAuthorizationToken(authHeader)){ return { status: false, code: 401, condition: 'token-expired', expired: true }; }
+    let query = await bd.statusValrepQuery().then((res) => res);
+    if(query.error){ return { status: false, code: 500, message: query.error }; }
+    let jsonArray = [];
+    for(let i = 0; i < query.result.recordset.length; i++){
+        jsonArray.push({ cestatusgeneral: query.result.recordset[i].CESTATUSGENERAL, xestatusgeneral: query.result.recordset[i].XESTATUSGENERAL });
+    }
+    return { status: true, list: jsonArray }
+}
+
 module.exports = router;
